f223c9a29908340aedb78dab0d080b32348dcf9f
Juan Camilo <juancamilopc1622@gmail.com>
2023-09-03 21:30:06 -0500

    se continuó con las view welcome y dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -85,22 +85,236 @@
         <div class="contenido-imperio2 container">
             <div class="row">
                 <div class="col-12 col-sm-12 col-md-12 col-lg-9">
-                <h3>El imperio de la <span>disciplina</span></h3>
-                <p>¡Bienvenidos al sitio web de Reich Gym!
-                    En Reich Gym, nuestra misión es ayudarte a alcanzar tus metas de acondicionamiento 
-                    físico y salud. Nuestro equipo de entrenadores altamente capacitados está comprometido
-                    para brindarte la mejor experiencia posible y así puedas alcanzar tus objetivos de forma 
-                    segura y eficiente.</p>
+                <h3>Transforma tu <span>vida</span></h3>
+                <p>Nuestra misión es guiarte hacia un cambio positivo en tu vida a través del ejercicio. Nuestro equipo de expertos en bienestar está aquí para apoyarte en tu camino hacia una vida más saludable y activa. Estamos comprometidos a proporcionarte las herramientas y el apoyo necesarios para que puedas transformar tu vida de manera segura y efectiva. ¡Juntos, lograremos tus metas de bienestar!.</p>
                 </div>
                 <div class="col-numero2 col-12 col-sm-12 col-md-12 col-lg-3">
                     <h2>02</h2>
                 </div>
-                <!-- <div class="col col-lg-3">
-                    <img src="{{ asset('images/recursos-pagina/personas-ejercicio.svg') }}" alt="Ejercicio gym" class="img-imperio img-responsive">
-                </div> -->
             </div>
         </div>
     </section>
+
+    <!-- Servicios section -->
+
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <section class="contenedor-introduccion-servicios container">
+            <div class="textos-introduccion-servicios">
+                <h3>Con nosotros</h3>
+                <h2 class="texto-recibes">Recibes:</h2>
+            </div>
+
+            <div class="cards-servicios-contenedor row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col col-sm-6">
+                        <div class="card h-100 w-100">
+                        <img src="{{asset('images/banner/banner1.jpg')}}" class="card-img-top img-fixed-size" alt="...">
+                        <div class="card-body">
+                            <p>Asesorías</p>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div class="col col-sm-6">
+                        <div class="card h-100 w-100">
+                        <img src="{{asset('images/banner/banner1.jpg')}}" class="card-img-top img-fixed-size" alt="...">
+                        <div class="card-body">
+                            <p>Entrenamiento semi-personalizado</p>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div class="col col-sm-6">
+                        <div class="card h-100 w-100">
+                        <img src="{{asset('images/banner/banner1.jpg')}}" class="card-img-top img-fixed-size" alt="...">
+                        <div class="card-body">
+                            <p>Suplementación</p>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div class="col col-sm-6">
+                        <div class="card h-100 w-100">
+                        <img src="{{asset('images/banner/banner1.jpg')}}" class="card-img-top img-fixed-size" alt="...">
+                        <div class="card-body">
+                            <p>Clases grupales</p>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div class="col col-sm-6">
+                        <div class="card h-100 w-100">
+                        <img src="{{asset('images/banner/banner1.jpg')}}" class="card-img-top img-fixed-size" alt="...">
+                        <div class="card-body">
+                            <p>Valoración física</p>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div class="col col-sm-6">
+                        <div class="card h-100 w-100">
+                        <img src="{{asset('images/banner/banner1.jpg')}}" class="card-img-top img-fixed-size" alt="...">
+                        <div class="card-body">
+                            <p>Eventos y actividades sociales</p>
+                        </div>
+                        </div>
+                    </div>
+    </div>
+
+    <section class="zonas-especializadas">
+    <div class="texto-zonas">
+    <h2>Zonas <span>especializadas:</span></h2>
+    </div>
+
+    <div class="cards-servicios-contenedor row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col col-sm-6">
+                        <div class="card h-100 w-100">
+                        <img src="{{asset('images/banner/banner1.jpg')}}" class="card-img-top img-fixed-size" alt="...">
+                        <div class="card-body">
+                            <p>Zona musculación</p>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div class="col col-sm-6">
+                        <div class="card h-100 w-100">
+                        <img src="{{asset('images/banner/banner1.jpg')}}" class="card-img-top img-fixed-size" alt="...">
+                        <div class="card-body">
+                            <p>Zona Cardio</p>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div class="col col-sm-6">
+                        <div class="card h-100 w-100">
+                        <img src="{{asset('images/banner/banner1.jpg')}}" class="card-img-top img-fixed-size" alt="...">
+                        <div class="card-body">
+                            <p>Salones grupales</p>
+                        </div>
+                        </div>
+                    </div>
+    </div>
+    </section> <!-- sección Zonas especializadas -->
+
+
+            </section>
+    </div>
+
+    <!-- Footer -->
+    <footer class="text-center text-lg-start bg-dark text-white">
+      <!-- Social media section-->
+      <section
+        class="social-media-section d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+      >
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+          <span>Visítanos en nuestras redes sociales:</span>
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div class="icons-social-media" style="display: flex; margin-top: 5px;">
+        <a href="https://www.instagram.com/reichgym._/" class="me-4 link-secondary">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=100078401858405" class="me-4 link-secondary">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="" class="me-4 link-secondary">
+            <i class="fab fa-twitter"></i>
+          </a>
+        </div>
+      </section>
+      <!-- Social media Section  -->
+
+      <!-- Links Section  -->
+      <section class="links-section">
+        <div class="container text-center text-md-start mt-5">
+          <!-- Grid row -->
+          <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <!-- Content -->
+              <h6 class="text-uppercase fw-bold mb-4">
+              <i class="fa-solid fa-dumbbell me-3"></i>Reich Gym
+              </h6>
+              <p>
+              En Reich Gym estamos comprometidos en proporcionarte una experiencia excepcional mientras te esfuerzas por mantenerte en forma y saludable. ¡Esperamos verte pronto en nuestras instalaciones!
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div
+              class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4"
+              style="text-align: center"
+            >
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">Membresías</h6>
+              @foreach ($membresiasData as $membresiasDatas)
+              <p class="py-1">
+                <a href="#!" class="text-reset">{{$membresiasDatas->NOMBREMEMBRESIA}}</a>
+              </p>
+              @endforeach
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div
+              class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4"
+              style="text-align: center"
+            >
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">Links</h6>
+              <p>
+                <a href="#!" class="text-reset">Sedes</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Settings</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Orders</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Help</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">Contacto</h6>
+              <p>
+                <i class="fas fa-home me-3"></i> Cualquiera de nuestras sedes
+              </p>
+              <p>
+                <i class="fas fa-envelope me-3"></i>
+                Reichgym@gmail.com
+              </p>
+              <p>
+                <i class="fas fa-phone me-3"></i> 3228178509
+              </p>
+              <p>
+                <i class="fas fa-print me-3"></i> 3112562333
+              </p>
+            </div>
+            <!-- Grid column -->
+          </div>
+          <!-- Grid row -->
+        </div>
+      </section>
+      <!-- Section: Links  -->
+
+      <!-- Copyright -->
+      <div
+        class="text-center p-4"
+        style="background-color: rgba(0, 0, 0, 0.3)"
+      >
+        <b>© Reich Gym</b>
+      </div>
+      <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
 
     <script type="text/javascript" src="{{ asset('js/sedes.js') }}"></script>
 </x-app-layout>
--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -3,3 +3,6 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+<!-- FontAwesome -->
+<script src="https://kit.fontawesome.com/4a6729b17a.js" crossorigin="anonymous"></script>
