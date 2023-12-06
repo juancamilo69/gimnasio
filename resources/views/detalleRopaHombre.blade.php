2d9f633e440e7685f63b4d43b7dd264e242bd32b
Martin Rodriguez <martimol32@gmail.com>
2023-12-05 23:31:55 -0500

    Se crean los carruseles de detalles suplemntos y ropa

--- resources/views/detalleRopaHombre.blade.php
+++ resources/views/detalleRopaHombre.blade.php
@@ -30,9 +30,27 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="contenedor-producto">
-                            <div class="contenedor-img-producto">
-                                <img src="{{$prenda->IMGPRENDA1}}" class="card-img-top img-fixed-size" alt="...">
-                            </div>
+                            <div id="carouselExample" class="carousel slide">
+                                <div class="carousel-inner">
+                                  <div class="carousel-item active">
+                                    <img src="{{$prenda->IMGPRENDA1}}" class="d-block w-100" alt="...">
+                                  </div>
+                                  <div class="carousel-item">
+                                    <img src="{{$prenda->IMGPRENDA2}}" class="d-block w-100" alt="...">
+                                  </div>
+                                  <div class="carousel-item">
+                                    <img src="{{$prenda->IMGPRENDA3}}" class="d-block w-100" alt="...">
+                                  </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                  <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                  <span class="visually-hidden">Next</span>
+                                </button>
+                              </div>
                         </div>
                     </div>
                     <div class="col-md-6">
@@ -43,6 +61,7 @@
                                 <h3>{{ $prenda->COLOR }}<span> | </span>{{ $prenda->MATERIAL }}</h3>
                                 <p>{{ $prenda->DESCRIPCION }}</p>
                                 <h2 class="precio-producto">{{ number_format($prenda->PRECIO, 0, ',', '.') }}</h2>
+                                <button class="btn btn-dark btn-comprar mt-3" style="width: 50%">Comprar</button>
                             </div>
                         </div>
                     </div>
--- resources/views/detalleSuplemento.blade.php
+++ resources/views/detalleSuplemento.blade.php
@@ -30,9 +30,24 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="contenedor-producto">
-                            <div class="contenedor-img-producto">
-                                <img src="{{$suplemento->IMGSUPLEMENTO}}" class="card-img-top img-fixed-size" alt="...">
-                            </div>
+                            <div id="carouselExample" class="carousel slide">
+                                <div class="carousel-inner">
+                                  <div class="carousel-item active">
+                                    <img src="{{$suplemento->IMGSUPLEMENTO}}" class="d-block w-100" alt="...">
+                                  </div>
+                                  <div class="carousel-item">
+                                    <img src="{{$suplemento->IMGSUPLEMENTO}}}" class="d-block w-100" alt="...">
+                                  </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                  <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                  <span class="visually-hidden">Next</span>
+                                </button>
+                              </div>
                         </div>
                     </div>
                     <div class="col-md-6">
