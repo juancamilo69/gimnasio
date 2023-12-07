bc8eb7ba021f8fca595b1700c4a3c92e8870e4b1
Martin Rodriguez <martimol32@gmail.com>
2023-12-06 22:32:22 -0500

    Se cambia el alert al eliminar un elemento

--- resources/views/views-admin/elementos.blade.php
+++ resources/views/views-admin/elementos.blade.php
@@ -10,6 +10,9 @@
 <!-- Custom Css -->
 <link rel="stylesheet" href="{{ asset('css/elementos.css') }}">
 
+<!-- SweetAlert -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 <Section class="hero">
     <h5>Registro de elementos</h5>
 </Section>
@@ -49,7 +52,24 @@
                     <div class="btns-card">
                     <a href=""><button type="button" class="btns btn btn-light">{{$elemento->SOPORTE}}</button></a>
                     <a href="{{ route('elementos.edit', $elemento->IDELEMENTO) }}"><button type="button" class="btns btn btn-light btn-editar">Editar</button></a>
-                    <a href="{{route('elementos.destroy', $elemento->IDELEMENTO)}}" onclick="return confirm('¿Seguro que quiere eliminar este elemento?')"><button type="button" class="btns btn btn-light btn-eliminar">Eliminar</button></a>
+                    <button type="button" class="btns btn btn-light btn-eliminar" onclick="confirmDelete('{{route('elementos.destroy', $elemento->IDELEMENTO)}}')">Eliminar</button>
+                        <script>
+                        function confirmDelete(url) {
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Oops...',
+                                text: '¿Seguro que quiere eliminar este elemento?',
+                                showCancelButton: true,
+                                confirmButtonColor: '#3085d6',
+                                cancelButtonColor: '#d33',
+                                confirmButtonText: 'Sí, eliminarlo'
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    window.location.href = url;
+                                }
+                            });
+                        }
+                    </script>
                     </div>
                 </div>
             </div>
